365d3affe53c8d2bca29b636a4a33528657ce5d8
ramonriosm <ramonriosm@gmail.com>
2015-05-26 22:08:49 +0000

    create recipe views

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,6 +2,6 @@ class Chef < ActiveRecord::Base
   has_many :recipes
   before_safe { self.email = email.downcase }
   validates :chefname, presence: true, length: {minimum: 3, maximum: 40}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
   validates :email, presence: true, length: {maximum: 105}, uniqueness: {case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
 end
